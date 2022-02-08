1b8ed8c4771cabed1579270673ae7ceb09f03ac1
asmit-a <asmita@g.ucla.edu>
2022-02-07 23:53:29 -0800

    Small updates (commenting) to imdb_spider.py

--- IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -1,61 +1,46 @@
-# to run 
-# scrapy crawl imdb_spider -o movies.csv
-
 import scrapy
 from scrapy.http import Request
 
 class ImdbSpider(scrapy.Spider):
     name = 'imdb_spider'
     
-    # start url contains the url to arcane's IMDB page
-    start_urls = ['https://www.imdb.com/title/tt0417299/']
+    # Doctor Who (2005)'s IMDB page
+    start_urls = ['https://www.imdb.com/title/tt0436992/']
 
-    # assumes you start on a movie page, navigates to cast&crew page, 
-    # calls parse_full_credits
+
     def parse(self, response):
-        # need to define what url is
-        movie_url = "https://www.imdb.com/title/tt0417299/"
+        """
+        Assumes we start on a movie's main page and navigates to 
+        movie's Cast & Crew page. 
+
+        @param self: an instance of this class.  
+        @param response: the result of scrapy's HTTP request. 
+        """
+
+        movie_url = "https://www.imdb.com/title/tt0436992/"
         credits_url = movie_url + "fullcredits/"
         yield Request(credits_url, callback = self.parse_full_credits)
 
-    # assumes you start on cast&crew page, leads to page of each actor,
-    # calls parse_actor_page
     def parse_full_credits(self, response):
-        base_url = "https://www.imdb.com"
-        # need to retrieve list of all actors 
-        # cast_table = response.css("table.cast_list")
-        # cast_rows = cast_table.css("td")
-        # cast_names = cast_rows.css("a")
+        """
+        Assumes we start on a movie's Cast & Crew page and navigates
+        to the pages of each of the cast members listed on it. 
 
-        # cast_links = [link.attrib["href"] for link in cast_names]
-        # THINK ABOUT WHY THIS DOESN'T WORK.
-
+        @param self: an instance of this class.  
+        @param response: the result of scrapy's HTTP request. 
+        """
+        # retrieves all the relative paths to the actors
+        # listed on the Cast & Crew page
         actor_urls = [a.attrib["href"] for a in response.css("td.primary_photo a")]
-        # gives all the relative paths to the actors
-
+        base_url = "https://www.imdb.com"
+        
+        # loops through each of the relative actor urls
         for url in actor_urls: 
             full_actor_url = base_url + url
             yield Request(full_actor_url, callback = self.parse_actor_page)
 
-        # using css selectors
-
-        # need to loop through each actor
-        # and call a scrapy request
-
-    # assumes you start on page of actor, yields dictionary
     def parse_actor_page(self, response):
-        
-        # on the page of an actor
-
-        # need to determine name of actor
-
-        # retrieve each of the movies/tv shows they've worked on
-        # input to dictionary
-
-        # to get NAME: 
         name_string = response.css("td.name-overview-widget__section h1.header span::text").get()
-
-        # to get all of their FILMS:
         filmography_rows = response.css("div.filmo-row")
         filmography = [row.css("a::text").get() for row in filmography_rows]
 
@@ -65,6 +50,10 @@ class ImdbSpider(scrapy.Spider):
                 "movie_or_TV_name": film
             }
 
-        # yields dictionary
+
+
+# to run 
+# scrapy crawl imdb_spider -o movies.csv
+
 
         
